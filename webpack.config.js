db7deb5c9c65a599c5f604521a2da977c4a1d373
muddassar-lab <muddassarsaleem89@gmail.com>
2021-01-01 17:24:27 +0500

    added starter things for library.

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require("path");
+module.exports = {
+  entry: path.resolve(__dirname, "src/index.ts"),
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        include: [path.resolve(__dirname, "src")],
+      },
+    ],
+  },
+  output: {
+    library: "simpra",
+    libraryTarget: "umd",
+    filename: "simpra.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+};
